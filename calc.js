11598d85320bb8e4ba1885596e10747687088326
Dinesh korukonda <131236761+dinexh@users.noreply.github.com>
2024-10-18 12:45:01 +0530

    2300030350_Dinesh

---
+++ calc.js
@@ -0,0 +1,5 @@
+let arr=[1,4,9,16,25];
+
+let newar=arr.map((x)=>x*2);
+
+console.log(newar);
---
+++ hello.js
@@ -0,0 +1,24 @@
+// console.log("Welcome to ZeroOne");
+
+// let var const
+
+// const a =10;
+
+// a="hello";
+
+// console.log(a);
+
+// const add=(a,b)=>{
+//     return a+b;
+// }
+// console.log(add(5,5));
+
+
+
+
+let arr=[1,2,3,4,5];
+
+let newarr=arr.map((x)=> x*x);
+
+console.log(newarr);
+
